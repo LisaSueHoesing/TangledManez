2e8f8df9cbf75e4b46d396908cd926cd50a809df
eligilbert <eliasbgilbert@gmail.com>
2021-05-31 13:59:44 -0700

    contact back end odds and ends

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 const serv = require('http').Server(app);
 const io = require('socket.io')(serv, {});
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/styles'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'post.multimusic@gmail.com',
+        pass: 'Musician1'
+    }
+});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -38,6 +49,38 @@ app.get('/about', function(req, res) {
 
 app.get('/gallery', function(req, res) {
     res.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.get('/contacted', function(req, res) {
+    res.sendFile(__dirname + '/views/contacted.html');
+});
+
+app.get('/mailing', function(req, res) {
+    res.sendFile(__dirname + '/views/mailing.html');
+});
+
+app.post('/submit_contact_form', function(req, res) {
+    let mailOptions = {
+        from: req.body.Name + ' <post.multimusic@gmail.com>',
+        to: "lisasuehoesing@gmail.com",
+        replyTo: req.body.Name + ' <' + req.body.Email + '>',
+        subject: req.body.Subject,
+        text: req.body.Message
+    };
+    transporter.sendMail(mailOptions);
+    res.redirect('/contacted');
+});
+
+app.post('/submit_mailing_form', function(req, res) {
+    let mailOptions = {
+        from: 'TangledManeZ Webmaster <post.multimusic@gmail.com>',
+        to: "lisasuehoesing@gmail.com",
+        subject: 'TangledManeZ: Mailing List Request',
+        text: 'Hi Lisa,\n\nThe following person has requested to be added to the TangledManeZ mailing list:' +
+            '\n\n' + req.body.mailingemail + '\n\nBest,\nYour website'
+    };
+    transporter.sendMail(mailOptions);
+    res.redirect('/mailing');
 });
 
 const port = process.env.PORT || 8080;
